57b385d601d50acc7ac56764615659f8137efb03
Mrugesha Garambha <mrugeshavocsy@gmail.com>
2022-06-25 14:06:05 +0530

    init

--- lib/flutter_session.dart
+++ lib/flutter_session.dart
@@ -18,7 +18,7 @@ class FlutterSession {
   Map _session = {};
 
   // Yes, it uses SharedPreferences
-  SharedPreferences prefs;
+  SharedPreferences? prefs;
 
   // Initialize the SharedPreferences instance
   Future _initSharedPrefs() async {
@@ -32,9 +32,9 @@ class FlutterSession {
   Future get(key) async {
     await _initSharedPrefs();
     try {
-      return json.decode(this.prefs.get(key));
+      return prefs!.get(key);
     } catch (e) {
-      return this.prefs.get(key);
+      return this.prefs!.get(key);
     }
   }
 
@@ -51,42 +51,42 @@ class FlutterSession {
       // String
       case String:
         {
-          this.prefs.setString(key, value);
+          prefs!.setString(key, value);
         }
         break;
 
       // Integer
       case int:
         {
-          this.prefs.setInt(key, value);
+          prefs!.setInt(key, value);
         }
         break;
 
       // Boolean
       case bool:
         {
-          this.prefs.setBool(key, value);
+          prefs!.setBool(key, value);
         }
         break;
 
       // Double
       case double:
         {
-          this.prefs.setDouble(key, value);
+          prefs!.setDouble(key, value);
         }
         break;
 
       // List<String>
       case List:
         {
-          this.prefs.setStringList(key, value);
+          prefs!.setStringList(key, value);
         }
         break;
 
       // Object
       default:
         {
-          this.prefs.setString(key, jsonEncode(value.toJson()));
+          prefs!.setString(key, jsonEncode(value.toJson()));
         }
     }
 
